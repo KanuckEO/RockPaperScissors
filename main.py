046df97241d2cab7402069f6a19aeec57f3dda3b
KANSHAH <shahkanuck@gmail.com>
2021-01-14 18:15:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#KanuckEO
+
+n = input("Choose your material")
+print(n)
+print("Vs")
+import random
+r = random.randint(1 , 3)
+
+if r == 1:
+  print("Rock")
+
+if r == 2:
+  print("Paper")
+
+if r == 3:
+  print("Scrissors")
